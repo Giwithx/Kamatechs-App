e63af531010f7c264bfd8af45fc7efdef5c76a0f
Giwithx <bielzagio@gmail.com>
2022-12-05 15:16:02 +0800

    Beta Version 0.1 - Register and Login is working

--- app/src/main/java/com/example/kamatechsmobileapplication/MainActivity.kt
+++ app/src/main/java/com/example/kamatechsmobileapplication/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.kamatechsmobileapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.kamatechsmobileapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+        val actionbar = supportActionBar
+        actionbar!!.title = "Kamatechs"
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
